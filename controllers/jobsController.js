9674a7f6f1d6a28664756d22f6832aefd7a5a9bc
Romulo91 <romulo.galiao@googlemail.com>
2023-10-25 22:27:01 +0100

    working on

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -23,8 +23,68 @@ exports.newJob = async (req, res, next) => {
   });
 };
 
-// Search jobs with radius => /api/v1/jobs/:zipcode/:distance
+// Get a single job with id and slug => /api/v1/job/:id/:slug
+exports.getJob = async (req, res, next) => {
+  const job = Job.find({
+    $and: [{ _id: req.params.id }, { slug: req.params.slug }],
+  });
 
+  if (!job || job.length === 0) {
+    return res.status(404).json({
+      success: false,
+      message: 'Job not found',
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    message: job,
+  });
+};
+
+// Update a Job => /api/v1/job/:id
+exports.updateJob = async (req, res, next) => {
+  let job = await Job.findById(req.params.id);
+
+  if (!job) {
+    return res.status(404).json({
+      success: false,
+      message: 'Job not found',
+    });
+  }
+
+  job = await Job.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: 'Job is updated',
+    data: job,
+  });
+};
+
+// Delete a Job => /api/v1/job/:id
+exports.deleteJob = async (req, res, next) => {
+  let job = await Job.findById(req.params.id);
+
+  if (!job) {
+    return res.status(404).json({
+      success: false,
+      message: 'Job not found',
+    });
+  }
+
+  job = await job.remove(req.params.id);
+
+  res.status(200).json({
+    success: true,
+    message: 'Job is deleted',
+  });
+};
+
+// Search jobs with radius => /api/v1/jobs/:zipcode/:distance
 exports.getJobsInRadius = async (req, res, net) => {
   const { zipcode, distance } = req.params;
 
--- routes/jobs.js
+++ routes/jobs.js
@@ -4,14 +4,21 @@ const router = express.Router();
 // import jobs controller methods
 const {
   getJobs,
+  getJob,
   newJob,
   getJobsInRadius,
+  updateJob,
+  deleteJob,
 } = require('../controllers/jobsController');
 
 router.route('/jobs').get(getJobs);
 
+router.route('/job/:id').get(getJob);
+
 router.route('/jobs/:zipcode/:distance').get(getJobsInRadius);
 
 router.route('/job/new').post(newJob);
+
+router.route('/job/:id').put(updateJob).delete(deleteJob);
 
 module.exports = router;
